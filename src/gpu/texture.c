1d01f2571c2e94a3d1ee4bc030469a4f9ab8b567
Christopher Lovell <15553561+gmfCoding@users.noreply.github.com>
2026-05-24 21:56:43 +0930

    Add GLCall wrappers to OpenGL functions in src/gpu/texture.c

--- src/gpu/texture.c
+++ src/gpu/texture.c
@@ -2,6 +2,8 @@
 #include "gpu/gpu_texture.h"
 #include "logging.h"
 #include "util.h"
+#include "gldebug.h"
+
 // typedef struct t_resource
 // {
 // 	const char *name;
@@ -28,34 +30,34 @@
 
 void gpu_texture_create(t_gpu_texture* gpu_tex, int width, int height, int colour)
 {
-	glGenTextures(1, &gpu_tex->id);
-	glBindTexture(GL_TEXTURE_2D, gpu_tex->id);
+	GLCall(glGenTextures(1, &gpu_tex->id));
+	GLCall(glBindTexture(GL_TEXTURE_2D, gpu_tex->id));
 	// set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));	
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 	
 	int* data = malloc(width * height * sizeof(int)); 
 	for (int i = 0; i < width * height; i++)
 	{
 		data[i] = colour; // Red with full alpha
 	}
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data));
 	free(data);
 }
 
 void gpu_texture_add(t_gpu_texture* gpu_tex, const char *name, const char *asset_path, int channels)
 {
 	UNUSED(name);
-	glGenTextures(1, &gpu_tex->id);
+	GLCall(glGenTextures(1, &gpu_tex->id));
 	te_logf(LOG_LEVEL_INFO, "gpu_texture", "Generated Texture: %u\n", gpu_tex->id);
-	glBindTexture(GL_TEXTURE_2D, gpu_tex->id);
+	GLCall(glBindTexture(GL_TEXTURE_2D, gpu_tex->id));
 	// set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));	
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 
 	stbi_set_flip_vertically_on_load(true);
 	// load and generate the texture
@@ -63,8 +65,8 @@ void gpu_texture_add(t_gpu_texture* gpu_tex, const char *name, const char *asset
 	unsigned char *data = stbi_load(asset_path, &width, &height, &nrChannels, channels);
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data));
+		GLCall(glGenerateMipmap(GL_TEXTURE_2D));
 	}
 	else
 	{
